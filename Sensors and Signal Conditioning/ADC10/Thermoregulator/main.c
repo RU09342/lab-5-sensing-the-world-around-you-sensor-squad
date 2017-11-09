b56bed224f804ca7e5480950a38baf3a46a5662d
Jessica <wozniakj7@students.rowan.edulslsgit config --global user.name Jessicagit config --global user.email wozniakj7@students.rowan.edu>
2017-11-09 13:46:28 -0500

    Updated Code adc10 temp sense

--- Sensors and Signal Conditioning/ADC10/Thermoregulator/main.c
+++ Sensors and Signal Conditioning/ADC10/Thermoregulator/main.c
@@ -1,14 +1,20 @@
 //Jessica Wozniak
 //Lab 5 Sensors: ADC10 MSP430G2553- Temperature Sensor
 //Created: 11/7/17
-//Last updated: 11/8/17
+//Last updated: 11/9/17
 
 #include <msp430.h>
+
+#define ADC10 BIT7
+#define LED1 BIT0
+#define RXD BIT1
+#define TXD BIT2
 
 void TimerInit(void);
 void ADC10Init(void);
 void UARTInit(void);
 
+//unsigned int tempC = 0
 long tempF = 0;
 long in = 0;
 
@@ -31,7 +37,9 @@ __interrupt void ADC10_ISR(void)
   ADC10CTL0 &= ~ENC;                        // ADC10 disabled
   ADC10CTL0 = 0;                            // ADC10, Vref disabled
   in = ADC10MEM;
-  tempF = ((in - 630) * 761) / 1024;
+//  tempC = in /100;
+//  tempF=9*tempC/5+32;
+  tempF = ((in - 630) * 761) / 1024;        //Conversion from ADC10 sample to temp F
 
   UCA0TXBUF = tempF;                        //send TempF to TX
 }
@@ -40,29 +48,15 @@ __interrupt void ADC10_ISR(void)
 __interrupt void Timer_A(void)
 
 {
-  ADC10CTL0 = SREF_1 + ADC10SHT_2 + REFON + ADC10ON + ADC10IE;
+  ADC10CTL0 = SREF_1 + ADC10SHT_2 + REFON + ADC10ON + ADC10IE;    // from TI exmaple code
   ADC10CTL0 |= ENC;                         // ADC10 enable set
 }
-
- #pragma vector=USCIAB0RX_VECTOR
- __interrupt void USCI0RX_ISR(void)
-
- {
-   while (!(IFG2&UCA0TXIFG));                // USCI_A0 TX buffer ready?
-
-   if (UCA0TXBUF){
-       UCA0TXBUF = ADC10MEM;
-   }
-   else{
-       UCA0TXBUF = 0;
-   }
- }
 //ADC Initialization
 void ADC10Init()
 {
       ADC10CTL1 = INCH_7 + SHS_1;             // P1.7, TA1 trigger sample start
-      ADC10AE0 = 0x80;                        // P1.7 ADC10 option select
-      P1DIR |= 0x01;                          // Set P1.0 to output direction
+      ADC10AE0 = ADC10;                                     // P1.7 ADC10 option select
+      P1DIR |= LED1;                                        // set LED1 to output
 }
 // Timer Initialization
 void TimerInit()
@@ -71,7 +65,7 @@ void TimerInit()
     TACCR0 = 4096-1;                          // PWM Period
     TACCTL1 = OUTMOD_3;                       // TACCR1 set/reset
     TACCR1 = 256;                             // TACCR1 PWM Duty Cycle
-    TACTL = TASSEL_1 + MC_1 + ID_3;           // ACLK, up mode
+    TACTL = TASSEL_1 + MC_1 + ID_3;           // ACLK, UP MODE, DIV 4
 }
 //UART Initialization
 void UARTInit()
@@ -81,8 +75,8 @@ void UARTInit()
         while(1);
       }
 
-    P1SEL = BIT1 + BIT2;                    // P1.1 = RXD, P1.2=TXD
-    P1SEL2 = BIT1 + BIT2 ;                  // P1.1 = RXD, P1.2=TXD
+      P1SEL = RXD + TXD;                    // P1.1 = RXD, P1.2=TXD
+      P1SEL2 = RXD + TXD;                  // P1.1 = RXD, P1.2=TXD
 
       DCOCTL = 0;                             // Select lowest DCOx and MODx settings
       BCSCTL1 = CALBC1_1MHZ;                  // Set DCO
@@ -93,6 +87,4 @@ void UARTInit()
       UCA0BR1 = 0;                            // 9600 baud
       UCA0MCTL = UCBRS0;                      // Modulation UCBRSx = 1
       UCA0CTL1 &= ~UCSWRST;                   // **Initialize USCI state machine**
-      IE2 |= UCA0RXIE;                          // Enable USCI_A0 RX interrupt
-
 }
