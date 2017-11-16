69fdecc77aa0a41866fdd4679fbc978a66229ee8
Jessica <wozniakj7@students.rowan.edulslsgit config --global user.name Jessicagit config --global user.email wozniakj7@students.rowan.edu>
2017-11-15 22:40:39 -0500

    updated code and code doc

--- Sensors and Signal Conditioning/ADC10/Phototransistor/main.c
+++ Sensors and Signal Conditioning/ADC10/Phototransistor/main.c
@@ -1,7 +1,7 @@
 //Jessica Wozniak & Ryan Hare
 //Lab 5 Sensors: ADC10 MSP430G2553- Phototransistor
 //Created: 11/7/17
-//Last updated: 11/10/17
+//Last updated: 11/15/17
 
 #include <msp430.h>
 
@@ -13,47 +13,39 @@
 void TimerInit(void);      //Timer function
 void ADC10Init(void);      //ADC10 function
 void UARTInit(void);       //UART function
+void ClockInit(void);      //clock Function
 
 unsigned int in = 0;
-char ADCMSB = 0;
-char ADCLSB = 0;
+float voltage;
+float current;
 
 int main(void)
 {
   WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
 
-  ADC10Init();                  //ADC10 Function
-  TimerInit();                  //Timer Function
-  UARTInit();                   //UART Function
+  ADC10Init();                  //ADC10 Function call
+  TimerInit();                  //Timer Function call
+  UARTInit();                   //UART Function call
+  ClockInit();                  //Clock Function call
 
   __bis_SR_register(GIE);       //interrupt enable
     while(1){
     }
-   }
+}
 //ADC ISR
 #pragma vector=ADC10_VECTOR
 __interrupt void ADC10_ISR(void)
 {
-    ADC10CTL0 &= ~ENC;                        // ADC10 disabled
-    ADC10CTL0 = 0;                            // ADC10, Vref disabled completely
     in = ADC10MEM;
-
-    ADCMSB = in >> 8;                   //shifts in to get msb
-    ADCLSB = in & 0xFF;                 //
-
-    UCA0TXBUF = ADCMSB;                  //sends MSB to TX
-    while(!(IFG2 & UCA0TXIFG));          //waits for tx to be clear
-    UCA0TXBUF = ADCLSB;                  //sneds the LSB to TX
-
-
-
+    voltage = in * 0.0033;                //Converts ADC to voltage. (Vref/2^10) = 0.0033 * ADC = voltage
+    current = voltage / 1000;             //Using ohms law to find current
 }
 //Timer ISR
 #pragma vector=TIMER0_A0_VECTOR
 __interrupt void Timer_A(void)
 
 {
-  ADC10CTL0 = SREF_1 + ADC10SHT_2 + REFON + ADC10ON + ADC10IE;
+  ADC10CTL0 = SREF_0 + ADC10SHT_2 + REFON + ADC10ON + ADC10IE;
   // VR+ = VREF+ and VR- = AVSS, 16 x ADC10CLKs, ADC10 Reference on, ADC10 On/Enable, ADC10 Interrupt Enable - from TI exmaple code
   ADC10CTL0 |= ENC;                         // ADC10 enable set
 }
@@ -84,13 +76,15 @@ void UARTInit()
       P1SEL = RXD + TXD;                    // P1.1 = RXD, P1.2=TXD
       P1SEL2 = RXD + TXD;                  // P1.1 = RXD, P1.2=TXD
 
-      DCOCTL = 0;                             // Select lowest DCOx and MODx settings
-      BCSCTL1 = CALBC1_1MHZ;                  // Set DCO
-      DCOCTL = CALDCO_1MHZ;
-
       UCA0CTL1 |= UCSSEL_2;                   // SMCLK
       UCA0BR0 = 104;                          // 9600 baud
       UCA0BR1 = 0;                            // 9600 baud
       UCA0MCTL = UCBRS0;                      // Modulation UCBRSx = 1
       UCA0CTL1 &= ~UCSWRST;                   // **Initialize USCI state machine**
+}
+void ClockInit()
+{
+    DCOCTL = 0;                             // Select lowest DCOx and MODx settings
+    BCSCTL1 = CALBC1_1MHZ;                  // Set DCO
+    DCOCTL = CALDCO_1MHZ;
 }
