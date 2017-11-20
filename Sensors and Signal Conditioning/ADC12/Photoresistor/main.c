bcc4e4140bd08c74497425b69bef967c5b86b9fa
Jessica <wozniakj7@students.rowan.edulslsgit config --global user.name Jessicagit config --global user.email wozniakj7@students.rowan.edu>
2017-11-20 00:45:10 -0500

    Added code ADC12 photoresistor

--- Sensors and Signal Conditioning/ADC12/Photoresistor/main.c
+++ Sensors and Signal Conditioning/ADC12/Photoresistor/main.c
@@ -1,11 +1,11 @@
 //Jessica Wozniak & Ryan Hare
-//Lab 5 Sensors: ADC10 MSP430G2553- Photoresistor
+//Lab 5 Sensors: ADC12 MSP43FR6989- Photoresistor
 //Created: 11/7/17
-//Last updated: 11/10/17
+//Last updated: 11/20/17
 
 #include <msp430.h>
 
-#define ADC12 BIT4          //define ADC12 as BIT4
+#define ADC12 BIT7          //define ADC12 as BIT4
 #define LED1 BIT0           //define LED1 as BIT0
 #define RXD BIT0            //define RXD as BIT0
 #define TXD BIT1            //define TXD as BIT1
@@ -13,62 +13,52 @@
 void TimerInit(void);      //Timer function
 void ADC12Init(void);      //ADC10 function
 void UARTInit(void);       //UART function
-void GPIOInit(void);
-void ClockInit(void);
+void GPIOInit(void);       //GPIO function
+void ClockInit(void);      //clock function
 
-unsigned int in = 0;
-char ADCMSB = 0;
-char ADCLSB = 0;
-
-unsigned int resistance = 0;
-float voltage = 0;
+unsigned volatile int in;
+volatile float voltage;
+volatile float resistance;
 
 int main(void)
 {
   WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
+  PM5CTL0 &= ~LOCKLPM5;         //disable HIGH-Z mode
 
-  ADC12Init();                  //ADC10 Function
-  TimerInit();                  //Timer Function
-  UARTInit();                   //UART Function
-  GPIOInit();                   //GPIO Function
-  ClockInit();                  //Clock Function
+  GPIOInit();                   //GPIO Function Call
+  TimerInit();                  //Timer Function Call
+  UARTInit();                   //UART Function Call
+  ClockInit();                  //Clock function Call
 
-  PM5CTL0 &= ~LOCKLPM5;
+  while(REFCTL0 & REFGENBUSY);              // If ref generator busy, WAIT
+   REFCTL0 |= REFVSEL_0 | REFON;             // Select internal ref = 1.2V
+                                             // Internal Reference ON
+  ADC12Init();                  //ADC10 Function Call
 
-  __bis_SR_register(GIE);      // Global Interrupt Enable
+  while(!(REFCTL0 & REFGENRDY));            // Wait for reference generator to settle
 
-    while(1){
-    }
+  while(1){
+      __bis_SR_register(LPM0_bits + GIE);      // LPM0, ADC12_ISR will force exit
+      __no_operation();
+  }
 }
 //ADC ISR
 #pragma vector = ADC12_VECTOR
 __interrupt void ADC12_ISR(void)
 {
-    ADC12CTL0 &= ~ADC12ENC;
-    ADC12CTL0 = 0;
-
     in = ADC12MEM0;
+    voltage = in * 0.000293;                    //converts ADC to voltage
+    resistance=(3300.0/voltage) - 1000;         //Using ohms law we can find resistance
 
-    ADCMSB = in >> 8;                      //shifts in to get msb
-    ADCLSB = in & 0xFF;                    // ADCLSB is an int (8 bits) so in is truncated to 8 bits giving us LSB
-
-    UCA0TXBUF = ADCMSB;                  //send MSB to TX
-    while(!(UCA0IFG&UCTXIFG));          // Waits for TX to be cleared
-    UCA0TXBUF = ADCLSB;                  //sends LSB to TX
-
-
-    voltage = in/1000;                               //mV to V
-    resistance= (3300.0/volt) - 1000;               //using voltage divider equation to get phototransistor value
-    while(!(UCA0IFG&UCTXIFG));
-    UCA0TXBUF = resistance;                        //send resistace valeu to TX
+    while(!(UCA0IFG&UCTXIFG));            //wait for TX to clear
+    UCA0TXBUF = resistance;              //send resistance to TX
 }
-
 //Timer ISR
 #pragma vector=TIMER0_A0_VECTOR
 __interrupt void Timer_A(void)
 
 {
-    ADC12CTL0 |= ADC12ENC | ADC12SC;
+    ADC12CTL0 |= ADC12SC | ADC12ENC;         //sample/ enable
 }
 //ADC Initialization
 void ADC12Init()
@@ -77,16 +67,17 @@ void ADC12Init()
     ADC12CTL1 = ADC12SHP;                     // ADCCLK = MODOSC; sampling timer
     ADC12CTL2 |= ADC12RES_2;                  // 12-bit conversion results
     ADC12IER0 |= ADC12IE0;                    // Enable ADC conv complete interrupt
-    ADC12MCTL0 |= ADC12INCH_4;                // A1 ADC input select; Vref=1.2V
-    P1OUT = BIT0;                             // Clear LED to start
-
+    ADC12MCTL0 |= ADC12INCH_4 | ADC12VRSEL_1; // A1 ADC input select, Vref = 1.2V
+    P1OUT = LED1;                             // LED1 on
 }
 // Timer Initialization
 void TimerInit()
 {
     TA0CCTL0 = CCIE;                           // Enable interrupt
-    TA0CCR0 = 4096-1;                           // PWM Period
-    TA0CTL = TASSEL_1 + MC_1;                   // ACLK, CONT MODE
+    TA0CCR0 = 4096-1;                          // PWM Period
+    TA0CCTL1 = OUTMOD_3;                       // TACCR1 set/reset
+    TA0CCR1 = 256;                             // TACCR1 PWM Duty Cycle
+    TA0CTL = TASSEL_1 + MC_1 + ID_3;           // ACLK, CONT MODE
 }
 void ClockInit()
 {
@@ -112,8 +103,8 @@ void UARTInit()
 void GPIOInit(void)
 {
     // GPIO Setup
-    P1OUT &= ~BIT0;                           // Clear LED to start
-    P1DIR |= BIT0;                            // P1.0 output
-    P1SEL1 |= ADC12;                           // Configure P1.4 for ADC
-    P1SEL0 |= ADC12;
+    P1OUT &= ~LED1;                           // Clear LED to start
+    P1DIR |= LED1;                            // P1.0 output
+    P8SEL1 |= ADC12;                          // P8.7 for ADC
+    P8SEL0 |= ADC12;
 }
